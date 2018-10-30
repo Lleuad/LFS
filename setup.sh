7e9a2cebfd33a767e6556d5a9b11616345c249a1
Erik Nonhebel <Lleuad@users.noreply.github.com>
2018-10-30 13:37:35 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ mkdir -v $LFS/sources
 chmod -v a+wt $LFS/sources
 
 cp -r build_scripts $LFS/sources
+chmod +x $LFS/sources/build_scripts/*
 wget "http://www.linuxfromscratch.org/lfs/downloads/8.3/wget-list" --continue
 wget "http://www.linuxfromscratch.org/lfs/downloads/8.3/md5sums" --continue --directory-prefix=$LFS/sources
 #wget --input-file=wget-list --continue --directory-prefix=$LFS/sources
